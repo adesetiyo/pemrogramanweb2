501ab7c47acdb81bc7e9dae6bfaa27b2a7e1cbd9
adesetiyo <adesetiyo13@gmail.com>
2023-05-09 12:55:56 +0700

    Upload pertama

---
+++ admin/adm_home.php
@@ -0,0 +1,21 @@
+<?php
+  include '../template/adm_nav.php';
+?>
+
+<div class="container">
+  <h3>Selamat datang ADMIN di Sistem Informasi Mahasiswa UNDHA AUB</h3>
+  <p>Ini adalah halaman informasi bagi mahasiswa baru di Universitas Dharma AUB.</p>
+  <hr>
+
+  <h4>Peraturan ADMIN UNDHA AUB</h4>
+  <ol>
+    <li>Dilarang memeberikan akses Username dan Password pada siapapun.</li>
+    <li>Wajib melakukan checking pedataan Mahasiswa Baru.</li>
+    <li>Wajib melakukan update Daftar Mata Kuliah.</li>
+    <li>Wajib Melakukan update Daftar Dosen Pengajar.</li>
+  </ol>
+</div>
+
+<?php
+    include '../template/footer.php';
+?>
---
+++ template/footer.php
@@ -0,0 +1,19 @@
+</body>
+
+<style>
+.footer {
+  position: fixed;
+  bottom: 0;
+}
+</style>
+<!-- Footer -->
+<div class="footer">
+<footer class="page-footer blue" >
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3">© 2023 Copyright:
+        <a href="https://web.facebook.com/profile.php?id=100069925719173&sk=about"> AdeSetiyo.com</a>
+    </div>
+    <!-- Copyright -->
+</footer>
+<!-- Footer </div>
+</html>
